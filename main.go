40664fce74d4de2936bce837f32981f524b81686
Leon Ji <mgleon08@gmail.com>
2019-05-15 23:21:10 +0800

    Animate the Ball

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	const (
-		width     = 12
+		width     = 50
 		height    = 12
 		cellEmpty = ' '
 		cellBall  = '⚾'
 	)
 
-	// rune == int32 == ''
-	var cell rune
+	var (
+		px, py    int
+		vx, vy    = 1, 1 // velocities
+		cell      rune   // rune == int32 == ''
+		maxFrames = 1200
+		speed     = time.Second / 20
+	)
 
 	// create board
 	board := make([][]bool, width)
@@ -23,11 +29,32 @@ func main() {
 
 	buf := make([]rune, width*height)
 
-	board[0][0] = true
+	// clear the screen
+	fmt.Print("\033[2J")
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < maxFrames; i++ {
+		// remove the previous ball
+		board[px][py] = false
+
+		// move x, y
+		px += vx
+		py += vy
+
+		// when the ball hits a border reverse its direction
+		if px <= 0 || px >= width-1 {
+			vx *= -1
+		}
+		if py <= 0 || py >= height-1 {
+			vy *= -1
+		}
+
+		// put the new ball
+		board[px][py] = true
+
 		// reuses buffer
 		buf = buf[:0]
+
+		// draw the board
 		for y := range board[0] {
 			for x := range board {
 				cell = cellEmpty
@@ -38,6 +65,12 @@ func main() {
 			}
 			buf = append(buf, '\n')
 		}
+
+		// move top left
+		fmt.Print("\033[H")
 		fmt.Println(string(buf))
+
+		// slow down the animation
+		time.Sleep(speed)
 	}
 }
